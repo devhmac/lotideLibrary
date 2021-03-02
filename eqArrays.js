f6655e175b125d236a5c509c2305e9ee96e85e36
Devin MacGillivray <devhmac@gmail.com>
2021-03-02 22:48:48 +0000

    added assertEqual function to be modified

---
+++ eqArrays.js
@@ -0,0 +1,7 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅ Assertion Passed: ${actual} === ${expected} ✅ ✅ `);
+  } else {
+    console.log(`🛑🛑 Assertion Failed: ${actual} !== ${expected} 🛑🛑`);
+  }
+};
