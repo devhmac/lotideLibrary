7651cedb9c74ddc5cd89d9ae809b6ef6b9cade6d
Devin MacGillivray <devhmac@gmail.com>
2021-03-03 21:48:56 +0000

    letterPositions functionality added

---
+++ letterPositions.js
@@ -0,0 +1,21 @@
+
+
+
+
+// letterPositions functionality
+const letterPositions = function (string) {
+  returnObj = {};
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] !== ' ') {
+      if (!returnObj[string[i]]) {
+        returnObj[string[i]] = [i];
+      } else {
+        returnObj[string[i]].push(i);
+      }
+    }
+  }
+  return returnObj
+};
+
+console.log(letterPositions('hello'))
+console.log(letterPositions('hi there'))
