f2fdc2e5e755b69f52a4778839b102b5cdd19932
Devin MacGillivray <devhmac@gmail.com>
2021-03-03 21:17:18 +0000

    refactored a conditional statment to remove redundancy

--- countOnly.js
+++ countOnly.js
@@ -13,14 +13,14 @@ const assertEqual = function (actual, expected) {
 
 
 const countOnly = function (allItems, itemsToCount) {
-  const returnObj = {}
+  const returnObj = {};
 
   for (let i = 0; i < allItems.length; i++) {
     //console.log('allItems[i]', allItems[i]);
     for (let key in itemsToCount) {
       //console.log('key', key);
       //console.log(itemsToCount[key])
-      if (allItems[i] === key && itemsToCount[key] === true) {
+      if (allItems[i] === key && itemsToCount[key]) {
         if (returnObj[key] === undefined) {
           returnObj[key] = 1;
         } else {
