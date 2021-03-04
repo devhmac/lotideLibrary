28f597d61354d1782e7cf34f1e0e5ab621c791c9
Devin MacGillivray <devhmac@gmail.com>
2021-03-04 21:36:41 +0000

    changing order of file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,9 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed: [actual] === [expected]");
+  } else console.log("Assertion Failed: [actual] != [expected]");
+}
+
 const eqArrays = function (array1, array2) {
   if (array1.length === array2.length) {
     for (let i = 0; i < array1.length; i++) {
@@ -10,12 +16,6 @@ const eqArrays = function (array1, array2) {
     return false;
   }
 };
-
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log("Assertion Passed: [actual] === [expected]");
-  } else console.log("Assertion Failed: [actual] != [expected]");
-}
 
 const assertArraysEqual = function (actual, expected) {
   if (eqArrays(actual, expected)) {
