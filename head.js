c372debffba5f4133fd480aba1069b44ec6a0574
Devin MacGillivray <devhmac@gmail.com>
2021-03-08 16:54:41 +0000

    exported head and added test/headTest

--- head.js
+++ head.js
@@ -5,9 +5,4 @@ const head = function (array) {
   return array[0];
 };
 
-
-
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined)
-assertEqual(head([1]), 1)
+module.exports = head
