1691a49a38ef6eb67744631024465de0d074f9cb
Devin MacGillivray <devhmac@gmail.com>
2021-03-08 20:29:57 +0000

    headTest completed

--- test/headTest.js
+++ test/headTest.js
@@ -1,15 +1,6 @@
 const assert = require('chai').assert;
 const head = require("../head");
 
-
-//our ugly test code not using mocha/chai
-
-
-
-// assertEqual(head([]), undefined)
-// assertEqual(head([1]), 1)
-
-//using mocha chai -------
 describe('head', () => {
   it('returns 1 for [1,2,3]', () => {
     assert.strictEqual(head([1, 2, 3]), 1);
