58d4a985ae0d663a9351e908e390871b602799c2
Devin MacGillivray <devhmac@gmail.com>
2021-03-08 19:52:54 +0000

    added test for assertArraysEqual in test/

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -8,12 +8,4 @@ const assertArraysEqual = function (actual, expected) {
   }
 };
 
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-
-assertArraysEqual([1, 2, 3], [1, 1, 2]);
-assertArraysEqual([1, 2, 3, 4], [1, 2, 3]);
-assertArraysEqual(['hi', 'hello', 'shalom'], ['hi', 'hello', 'shalo']);
-assertArraysEqual(['hi', 'hello', 'shalom'], ['hi', 'hello', 'shalom']);
-
 module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,10 @@
+const assertArraysEqual = require("../assertArraysEqual");
+
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+
+assertArraysEqual([1, 2, 3], [1, 1, 2]);
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3]);
+assertArraysEqual(['hi', 'hello', 'shalom'], ['hi', 'hello', 'shalo']);
+assertArraysEqual(['hi', 'hello', 'shalom'], ['hi', 'hello', 'shalom']);
