b80d43ce5315db3d98c152e7215c8301cadcf081
Devin MacGillivray <devhmac@gmail.com>
2021-03-08 20:48:33 +0000

    switched middle testing to mocha tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,18 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require('chai').assert
 const middle = require("../middle");
 
+describe('middle', () => {
+  it('should return [2,3] from [1,2,3,4]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it('should return [] from [2,3] as there is no middle', () => {
+    assert.deepEqual(middle([2, 3]), []);
+  });
+  it('should return [] from [1] as there is no middle', () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it('should return [3,4] from [1,2,3,4,5,6]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2,]), []);
-assertArraysEqual(middle([1,]), []);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+});
