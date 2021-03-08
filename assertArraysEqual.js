a9ed8b2f34c98f0d6fcf79369a234be92366af76
Devin MacGillivray <devhmac@gmail.com>
2021-03-08 19:51:41 +0000

    refactor assertArraysEqual to require eqArrays, instead of having function copied in file.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,4 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log("Assertion Passed: [actual] === [expected]");
-  } else console.log("Assertion Failed: [actual] != [expected]");
-}
-
-const eqArrays = function (array1, array2) {
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function (actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -32,3 +15,5 @@ assertArraysEqual([1, 2, 3], [1, 1, 2]);
 assertArraysEqual([1, 2, 3, 4], [1, 2, 3]);
 assertArraysEqual(['hi', 'hello', 'shalom'], ['hi', 'hello', 'shalo']);
 assertArraysEqual(['hi', 'hello', 'shalom'], ['hi', 'hello', 'shalom']);
+
+module.exports = assertArraysEqual;
