c37ebf71878e2f8dc8c01138a6c5d75aa3f03da1
Devin MacGillivray <devhmac@gmail.com>
2021-03-08 20:02:07 +0000

    refactored middle to export middle, and test in middleTest

--- middle.js
+++ middle.js
@@ -1,28 +1,7 @@
-// ASSERT EQUAL ARRAYS FUNCTION
 
-const eqArrays = function (array1, array2) {
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
-};
 
-const assertArraysEqual = function (actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅ Assertion Passed: ${actual} === ${expected} ✅✅ `);
-  } else {
-    console.log(`🛑🛑 Assertion Failed: ${actual} !== ${expected} 🛑🛑 `);
-  }
-};
 //MIDDLE FUNCTION!
-//if array.length = odd - array.length % 2 !== 0
-//Math.floor(array.length/2)
+
 const middle = function (array) {
   let midArray = [];
   let midIndex = Math.floor(array.length / 2);
@@ -37,7 +16,5 @@ const middle = function (array) {
 };
 
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2,]), []);
-assertArraysEqual(middle([1,]), []);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+
+module.exports = middle;
