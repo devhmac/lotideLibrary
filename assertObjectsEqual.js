8381c577105d7d62dc44fa427f7cbeb0faa66526
Devin MacGillivray <devhmac@gmail.com>
2021-03-09 01:12:26 +0000

    completed assertObjectsEqual export and refactor

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,28 +1,11 @@
-//eq objects & eqArrays function bundle
+
 const eqArrays = require('./eqArrays')
-
-//eqObjects needs eqArrays
-const eqObjects = function (object1, object2) {
-  obj1Keys = Object.keys(object1);
-  obj2Keys = Object.keys(object2);
-
-  if (obj1Keys.length !== obj2Keys.length) {
-    return false;
-  }
-  for (let key of obj1Keys) {
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (!eqArrays(object1[key], object2[key])) return false
-    } else if (object1[key] !== object2[key]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqObjects = require('./eqObjects')
 
 //assertObjectsEqual function
 const assertObjectsEqual = function (actual, expected) {
   const inspect = require('util').inspect; // this is to stop [Object Object] === [Object Object]  
-  // console.log(`Example label: ${inspect(actual)}`);
+
 
   if (eqObjects(actual, expected)) {
     console.log(`✅✅ Equality Passed: ${inspect(actual)} === ${inspect(expected)} ✅✅ `);
--- test/assertObjectsEqualTest.js
+++ test/assertObjectsEqualTest.js
@@ -1,4 +1,4 @@
-
+const assertObjectsEqual = require('../assertObjectsEqual');
 
 
 //testing!
